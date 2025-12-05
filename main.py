b7f7ad960249f96229eb1b8c6b23c57ad1dd0f19
laraldana77 <aldanalara97@gmail.com>
2025-12-04 22:34:32 -0300

    cambios

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+from modulos.socios import menu_socios
+from modulos.profesores import menu_profesores
+from modulos.pagos import menu_pagos
 
+def menu_principal():
+    while True:
+        print("\n=== SISTEMA DE GESTIÓN DE GIMNASIO ===")
+        print("1. Gestión de Clientes")
+        print("2. Gestión de profesores")
+        #print("2. Gestión de Rutinas")
+        print("3. Gestión de Pagos")
+        print("4. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            menu_socios()
+        elif opcion == "2":
+            menu_profesores()
+        elif opcion == "3":
+            menu_pagos()
+        elif opcion == "4":
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opción inválida. Intente nuevamente.")
+
+
+    menu_principal()
