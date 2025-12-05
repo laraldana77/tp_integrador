d4fcaf897c0b8a75466d2150beb57b3781a9d935
laraldana77 <aldanalara97@gmail.com>
2025-12-05 08:18:37 -0300

    actividades

--- modulos/actividades.py
+++ modulos/actividades.py
@@ -1,2 +1,10 @@
+import csv
+import os
+ARCHIVO_ACTIVIDADES="datos/actividades.csv"
 
-        
+def listar_actividades():
+    
+    with open(ARCHIVO_ACTIVIDADES, "r") as archivo:
+     reader=csv.reader(archivo)
+     for fila in reader:
+      print(f"Actividad: {fila[0]}- Horarios:{fila[1]}- Monto mensual:{fila[2]}")
